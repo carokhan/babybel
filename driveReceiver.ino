f5ed0e8b6c662b026a7323126e909e3a90ad98b2
Apoorva Saurav <hcps-saurava@protonmail.com>
2022-05-20 02:34:38 -0400

    maybe this works idk will test in the morning but input scaling ??

--- driveReceiver.ino
+++ driveReceiver.ino
@@ -1,15 +1,11 @@
 #include <Servo.h>
 #include <IBusBM.h>
-// int pwmLeft = 5;
-// int pwmRight = 6;
-int pwm = 2000;
 
 // Reading from receiver? https://medium.com/@werneckpaiva/how-to-read-rc-signal-with-arduino-using-flysky-ibus-73448bc924eb
 // https://www.instructables.com/RC-Control-and-Arduino-A-Complete-Works/
 
 // Outputting to Victors: https://github.com/FRC4014/SRTester/blob/master/SRTester.ino
 
-// int pwmPins[] = {5, 6};
 int pwmLeft = 6;
 int pwmRight = 5;
 Servo controllers[2];
@@ -30,7 +26,7 @@ void setup() {
     pinMode(digitalInR2, INPUT);
     controllers[0].attach(pwmLeft);
     controllers[1].attach(pwmRight);
-    Serial.println("BYE!");
+    Serial.println("T-minus 20 seconds");
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(20000);
     digitalWrite(LED_BUILTIN, LOW);
@@ -38,13 +34,25 @@ void setup() {
 
 void loop() {
     Serial.println("------");
-    // Serial.println("Test");
     vals[0] = pulseIn(digitalInL3, HIGH);
     vals[1] = pulseIn(digitalInR2, HIGH);
     Serial.println("Pin 7 right channel 2 "); Serial.println(valR);
     Serial.println("Pin 8 left channel 3 "); Serial.println(valL);
     Serial.println("------");
     for (int i = 0; i < 2; i++) {
-      controllers[i].write(vals[i]);
-    }
+      if (vals[i] > 1450 && vals[i] < 1550) {
+        vals[i] = 0;
+      } 
+      if (vals[i] > 1815) {
+        vals[i] = 2000;
+      }
+      if (vals[i] < 1105) {
+        vals[i] = 1000;
+      }
+      if (vals[i] > 1500)
+        controllers[i].write(ceil(vals[i]*1.1));
+      }
+      else {
+        controllers[i].write(floor(vals[i]/1.1));
+      }
 }
